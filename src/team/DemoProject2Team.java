e043039f26d96d836ef34d2e56e25dd85c9ac377
Yehyia Aljuboory <Yehyia Aljuboory@DESKTOP-JNNUE2G>
2024-01-05 19:41:48 -0600

    initial 01

---
+++ src/team/DemoProject2Team.java
@@ -0,0 +1,22 @@
+package team;
+
+public class DemoProject2Team {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test 1");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
